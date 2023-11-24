fdd2be07d1807c2f39e0373c2dc47ab94f434ebe
Chaooohs <01nikka01@gmail.com>
2023-11-24 14:19:40 +0200

    adsf

--- src/components/nav-list/nav-list.jsx
+++ src/components/nav-list/nav-list.jsx
@@ -1,18 +1,45 @@
 import { NavLink } from "react-router-dom"
+import { useMediaQuery } from 'react-responsive'
 import "./nav-list.css"
 
 export const NavList = ({ isOpen }) => {
+  const isTablet = useMediaQuery({ maxWidth: 992 })
+  const isDesktop = useMediaQuery({ minWidth: 993 })
+
   return (
-    <div className="list">
-      <NavLink className="link" to="store" onClick={() => isOpen(!isOpen)}>
-        Магазин
-      </NavLink>
-      <NavLink className="link" to="restaurant" onClick={() => isOpen(!isOpen)}>
-        Ресторан
-      </NavLink>
-      <NavLink className="link" to="video" onClick={() => isOpen(!isOpen)}>
-        Відео хостинг
-      </NavLink>
-    </div>
+    <>
+      {
+        isTablet &&
+        <div className="list">
+          <NavLink className="link" to="/" onClick={() => isOpen(!isOpen)}>
+            Головна
+          </NavLink>
+          <NavLink className="link" to="store" onClick={() => isOpen(!isOpen)}>
+            Магазин
+          </NavLink>
+          <NavLink className="link" to="restaurant" onClick={() => isOpen(!isOpen)}>
+            Ресторан
+          </NavLink>
+          <NavLink className="link" to="video" onClick={() => isOpen(!isOpen)}>
+            Відео хостинг
+          </NavLink>
+        </div>
+      }
+      {
+        isDesktop &&
+        <div className="list">
+          <NavLink className="link" to="store" onClick={() => isOpen(!isOpen)}>
+            Магазин
+          </NavLink>
+          <NavLink className="link" to="restaurant" onClick={() => isOpen(!isOpen)}>
+            Ресторан
+          </NavLink>
+          <NavLink className="link" to="video" onClick={() => isOpen(!isOpen)}>
+            Відео хостинг
+          </NavLink>
+        </div>
+      }
+    </>
+
   )
 }
